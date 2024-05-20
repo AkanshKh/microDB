2128e85f5e651d5d77190e66a65b376bfc6c7ea2
Akansh Khandelwal <128902296+AkanshKh@users.noreply.github.com>
2024-05-20 17:23:56 +0530

    giving segfault rn

--- includes/containers.cpp
+++ includes/containers.cpp
@@ -14,13 +14,13 @@ void print_prompt(){
     std::cout<<"db > ";
 }
 
-void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer);
-    free(input_buffer);
+void close_input_buffer(InputBuffer& input_buffer) {
+    free(input_buffer.buffer);
+    free(&input_buffer);
 }
 
-void read_input(InputBuffer * input_buffer){
-    ssize_t bytes_read = getline(&(input_buffer->buffer),&(input_buffer->buffer_length),stdin);
+void read_input(InputBuffer& input_buffer){
+    ssize_t bytes_read = getline(&(input_buffer.buffer),&(input_buffer.buffer_length),stdin);
 
     if(bytes_read <= 0){
         std::cout<<"Error reading input\n";
@@ -29,7 +29,119 @@ void read_input(InputBuffer * input_buffer){
 
 
     // Remove the newline character 
-    input_buffer->buffer_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read - 1] = 0;
+    input_buffer.buffer_length = bytes_read - 1;
+    input_buffer.buffer[bytes_read - 1] = 0;
     // std::cout<<input_buffer->buffer<<" "<<input_buffer->buffer_length<<std::endl;
+}
+
+MetaCommandResult do_meta_ccommand(InputBuffer& input_buffer, Table& table){
+    if(strcmp(input_buffer.buffer,".exit") == 0){
+        close_input_buffer(input_buffer);
+        free_table(table);
+        exit(EXIT_SUCCESS);
+    }
+    else{
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer& input_buffer, Statement& statement){
+    if(strncmp(input_buffer.buffer, "insert", 6) == 0){
+        statement.type = STATEMENT_INSERT;
+        int args_assigned = sscanf(input_buffer.buffer, "insert %u %s %s", statement.row_to_insert.id, statement.row_to_insert.username, statement.row_to_insert.email);
+
+        if(args_assigned < 3){
+            return PREPARE_SYNTAX_ERROR;
+        }
+        return PREPARE_SUCCESS;
+    }
+    if(strcmp(input_buffer.buffer, "select") == 0){
+        statement.type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void serialize_row(Row& source, void* destination){
+    memcpy(destination + ID_OFFSET, &source.id, ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &source.username, USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &source.email, EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row& destination){
+    memcpy(&destination.id, source + ID_OFFSET, ID_SIZE);
+    memcpy(&destination.username, source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&destination.email, source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void* row_slot(Table& table, uint32_t row_num){
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table.pages[page_num];
+
+    if(page == NULL){
+        page = table.pages[page_num] = malloc(PAGE_SIZE);
+    }
+
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+
+    return page + byte_offset;
+}
+
+void print_row(Row& row){
+    std::cout<<row.id<<" "<<row.username<<" "<<row.email<<std::endl;
+}
+
+ExecuteResult execute_insert(Statement& statement, Table& table){
+    if(table.num_rows >= TABLE_MAX_ROWS){
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row* row_to_insert = &statement.row_to_insert;
+
+    serialize_row(*row_to_insert, row_slot(table,table.num_rows));
+    table.num_rows++;
+
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement& statement, Table& table){
+    Row row;
+    for(uint32_t i = 0; i < table.num_rows; i ++){
+        deserialize_row(row_slot(table, i), row);
+        print_row(row);
+    }
+    
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement& statement, Table& table){
+    if(statement.type == STATEMENT_INSERT){
+        return execute_insert(statement,table);
+    }
+    if(statement.type == STATEMENT_SELECT){
+        return execute_select(statement,table);
+    }
+
+    std::cout<<"GAY GAY"<<std::endl;
+}
+
+Table* new_table(){
+    Table* table = new Table;
+
+    table->num_rows = 0;
+    for(uint32_t i = 0; i < TABLE_MAX_PAGES; i ++){
+        table->pages[i] = nullptr;
+    }
+
+    return table;
+}
+
+void free_table(Table& table){
+    for(uint32_t i = 0; i < table.num_rows; i ++){
+        free(table.pages[i]);
+    }
+
+    free(&table);
 }
--- main.cpp
+++ main.cpp
@@ -3,18 +3,48 @@
 using namespace std;
 
 int main(int argc,char* argv[]){
+
+    Table* table = new_table();
+
     InputBuffer* input_buffer = new_input_buffer();
 
     while(true){
         print_prompt();
-        read_input(input_buffer);
+        read_input(*input_buffer);
 
-        if(strcmp(input_buffer->buffer , ".exit") == 0){
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+        if(input_buffer->buffer[0] == '.'){
+            MetaCommandResult got = do_meta_ccommand(*input_buffer, *table);
+
+            if(got == META_COMMAND_SUCCESS){
+                std::cout<<"Successfully Executed"<<std::endl;
+                continue;
+            }
+            if(got == META_COMMAND_UNRECOGNIZED_COMMAND){
+                std::cout<<"Unrecognized Command "<<input_buffer->buffer<<std::endl;
+                continue;
+            }
         }
-        else{
-            std::cout<<"Unrecognized command "<<input_buffer->buffer<<std::endl;
+
+        Statement statement;
+
+        PrepareResult prepared = prepare_statement(*input_buffer,statement);
+
+        if(prepared == PREPARE_SYNTAX_ERROR){
+            std::cout<<"Cannot parse the query due to syntax errors\n";
+            continue;
+        }
+        if(prepared == PREPARE_UNRECOGNIZED_STATEMENT){
+            std::cout<<"Unrecognized Command initials "<<input_buffer->buffer<<std::endl;
+            continue;
+        }
+
+        ExecuteResult executed = execute_statement(statement, *table);
+        
+        if(executed == EXECUTE_SUCCESS){
+            std::cout<<"SUCCESSSSSSSSSS HEHE YOU GAY\n";
+        }
+        if(executed == EXECUTE_TABLE_FULL){
+            std::cout<<"TABLE IS FULL GAY MAN\n";
         }
     }
 }   
